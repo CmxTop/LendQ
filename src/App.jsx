6864f2b9c12d7157d79a9748a6373029932c903e
OlaGreat <oladipupoolamilekan2@gmail.com>
2025-12-07 17:56:47 +0100

    update url path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 import HealthIndicator from './components/HealthIndicator'
 import Toast from './components/Toast'
 
-const API_URL = 'http://localhost:3001'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
 const socket = io(API_URL)
 
 class ErrorBoundary extends Component {
